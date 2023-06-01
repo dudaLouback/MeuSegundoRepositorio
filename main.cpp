111b1a4dd3e1d18492f92840e65302895e18096b
Maria Eduarda <maria.louback@aluno.cefet-rj.br>
2023-06-01 14:21:24 -0300

    testei

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void implementarAlice() {
+	cout << "Alice!";
+}
+
+void implementarBob() {
+	cout << "Bob!";
+}
+
+int main() {
+	return 0;
+}
